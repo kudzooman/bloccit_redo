bb608692547583d72b7982e3b1ee82da5052133d
Chris Elwood <kudzooman@gmail.com>
2015-07-09 12:35:21 -0400

    added adverts model, controller and modified views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708094500) do
+ActiveRecord::Schema.define(version: 20150709103540) do
+
+  create_table "advertisements", force: true do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "answers", force: true do |t|
     t.text     "body"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 require 'faker'
 
+10.times do 
+  Advertisement.create!(
+    title: Faker::Lorem.sentence,
+    copy: Faker::Lorem.sentence
+    )
+end
+
 50.times do 
   Post.create!(
     title: Faker::Lorem.sentence,
@@ -18,4 +25,5 @@ end
 puts "Seed finished"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
 
