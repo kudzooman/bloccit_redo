76b7ac28c8e2f18cb85dd471abb3adcd649235d9
Chris Elwood <kudzooman@gmail.com>
2015-07-11 14:03:01 -0400

    added create and edit to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -8,8 +9,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to posts_path
+    else
+      flash[:error] = "Nope. You messed up. Try again."
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to posts_path
+    else
+      flash[:error] = "Nope. You messed up. Try again."
+      render :edit
+    end
+  end
+
 end
