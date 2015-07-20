0c9a8abfd6a7da617793a0396b5a0c36efdb432b
Chris Elwood <kudzooman@gmail.com>
2015-07-20 06:47:03 -0400

    made updates to user and post models and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
 require 'faker'
 
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
 30.times do
   Question.create!(
     title: Faker::Lorem.sentence,
@@ -7,31 +26,39 @@ require 'faker'
     )
 end
 
-10.times do 
+10.times do
   Advertisement.create!(
     title: Faker::Lorem.sentence,
     copy: Faker::Lorem.sentence
     )
 end
 
-50.times do 
+50.times do
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
 end
 posts = Post.all
 
-100.times do 
+100.times do
   Comment.create!(
     post: posts.sample,
     body: Faker::Lorem.paragraph
     )
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: "kudzooman@gmail.com",
+  password: "kudzoo007"
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} advertisements created"
 puts "#{Question.count} questions created"
-
